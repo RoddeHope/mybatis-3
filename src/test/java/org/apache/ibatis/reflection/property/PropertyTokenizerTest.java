395a518674abcccbc0956c3666698de1ad1c472b
luoxiongjian <luoxiongjian@qipeipu.com>
2019-10-17 14:10:54 +0800

    1. 学习PropertyTokenizer，添加测试类，妥妥的

---
+++ src/test/java/org/apache/ibatis/reflection/property/PropertyTokenizerTest.java
@@ -0,0 +1,37 @@
+package org.apache.ibatis.reflection.property;
+
+import org.junit.jupiter.api.Test;
+
+class PropertyTokenizerTest {
+
+  @Test
+  void testPropertyTokenizer() {
+    PropertyTokenizer propertyTokenizer = new PropertyTokenizer("order[0].item[0].name");
+    boolean hasNext = propertyTokenizer.hasNext();
+    String name = propertyTokenizer.getName();
+    String index = propertyTokenizer.getIndex();
+    String indexedName = propertyTokenizer.getIndexedName();
+    String children = propertyTokenizer.getChildren();
+    System.out.println("==== first ====");
+    System.out.println("hasNext = " + hasNext);
+    System.out.println("name = " + name);
+    System.out.println("index = " + index);
+    System.out.println("indexedName = " + indexedName);
+    System.out.println("children = " + children);
+
+    PropertyTokenizer next = propertyTokenizer.next();
+    hasNext = next.hasNext();
+    name = next.getName();
+    index = next.getIndex();
+    indexedName = next.getIndexedName();
+    children = next.getChildren();
+    System.out.println("==== second ====");
+    System.out.println("hasNext = " + hasNext);
+    System.out.println("name = " + name);
+    System.out.println("index = " + index);
+    System.out.println("indexedName = " + indexedName);
+    System.out.println("children = " + children);
+
+  }
+
+}
