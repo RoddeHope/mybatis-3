b1c7c0a0e47077c5ce28242c5f4bced61a6d32e5
luoxiongjian <luoxiongjian@qipeipu.com>
2019-10-21 19:56:33 +0800

    CollectionWrapper很扯淡，只有三个方法

--- src/main/java/org/apache/ibatis/reflection/wrapper/CollectionWrapper.java
+++ src/main/java/org/apache/ibatis/reflection/wrapper/CollectionWrapper.java
@@ -23,6 +23,7 @@ import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.property.PropertyTokenizer;
 
 /**
+ * 针对集合
  * @author Clinton Begin
  */
 public class CollectionWrapper implements ObjectWrapper {
